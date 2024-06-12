fac953a048a838d086f058649f628d8ba7f32e17
MARLEY CLYFF BONIFACIO DA SILVA <marley_c_silva@estudante.sesisenai.org.br>
2024-06-12 11:43:45 -0300

    botão de excluir

--- src/components/ListaTarefa.jsx
+++ src/components/ListaTarefa.jsx
@@ -9,6 +9,14 @@ const tarefasReducer = (state, action) => {
             const atualizarTarefa = [...state]
             atualizarTarefa[action.payload].completed = true
             return atualizarTarefa
+        case 'excluir_tarefa':
+            /*vou colocar um filtro na lista de tarefas para identificar a posição
+             do item que vai ser excluido e comparar os outros itens da lista com o atual
+             para criar uma nova lista de tarefas sem o item que foi excluido*/
+            return state.filter((_, index /*pegando a posição*/) => index !== action.payload)
+        //item da posicao precisa ser diferente do dado atual que ta chamando a ção excluir
+        default: //se nao tiver fazendo nada
+            return state; //retornar a lista de tarefas normal     
     }
 }
 
@@ -25,6 +33,10 @@ export default function ListaTarefas() {
 
     const concluirTarefa = useCallback((index) => {
         dispatch({ type: 'concluir_tarefa', payload: index })
+    }, [])
+
+    const ExcluirTarefa = useCallback((index) => {
+        dispatch({ type: 'excluir_tarefa', payload: index })
     }, [])
 
     return (
@@ -52,6 +64,11 @@ export default function ListaTarefas() {
                             <>
                                 <button onClick={() => concluirTarefa(index)}>Concluir</button>
                             </>
+                        )}
+                        {tarefas && (
+                          <>
+                          <button onClick={() => ExcluirTarefa(index)}>X</button>
+                          </>  
                         )}
                     </li>
                 ))}
